96256225bc3a06ef72b31faf1e7139fbec84fb48
Fabricio Alves <fabricioavsc@gmail.com>
2019-05-31 12:38:01 -0300

    Aplicando o AJAX

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(
     function(){
         console.log("Pagina Carregada");
-        $("#txtCep").focusout(function(){
-            fetch
+        var cep = $("#txtCEP").val();
+        cep = cep.replace("-", "");
+        var urlStr = "https://viacep.com.br/ws/"+ cep +"/json/";
+
+            $("#txtCEP").focusout(function(){
+            $.ajax({
+                url : urlStr ,
+                type: "get",
+                dataType: "json",
+                success : function(data){
+                    console.log(data);
+                    $("#txtCidade").val(data.localidade);
+                    $("#txtEs").val(data.uf);
+                    $("#txtBairro").val(data.bairro);
+                    $("#txtRua").val(data.logradouro);
+                    $("#txtComp").val(data.complemento);
+                },
+                error: function(erro){
+                    console.error(erro);
+                }
+                
+
+            })
         })
     }
 )
